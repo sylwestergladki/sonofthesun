0d0a5a308224763be8361571ac99de1eff45c992
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-03 20:38:05 +0200

    initial gulp site

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,76 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+const $doc = $(document)
+const $main = $("#navbar")
+const $header = $('.start--js')
+const $mainbio = $('.mainbio--js')
+
+
+$(document).ready(function(){
+$('#navbar').hide();
+$('#start').on("click",function(){
+
+$('#navbar').show();
+   
+    $('html, body').animate({
+        scrollTop: $("#navbar").offset().top
+    },1000)
+    
+   
+});
+});
+
+$doc.on('scroll', function () {
+    var scrollPos = $doc.scrollTop(); //jak daleko przewineliśmy
+    var sectionOffset = $main.offset().top; //gdzie jest dany element, na jakiej wysokoscio od początku strony się zaczyna.
+    //console.log(scrollPos);
+    //console.log(sectionOffset);
+
+    if (scrollPos < sectionOffset) {
+      $header.css('opacity', 1 - 1.9 * scrollPos / sectionOffset);
+    }
+  });
+
+  $(document).ready(function(){
+    $('.mainbio--js').hide();
+    $('.navbar__a--js1').on("click",function(){
+    
+    $('.mainbio--js').show();
+       
+        $('html, body').animate({
+            scrollTop: $(".mainbio--js").offset().top
+        },1000)
+        
+       
+    });
+    });
+
+    $(document).ready(function(){
+        $('.mainexp--js').hide();
+        $('.navbar__a--js2').on("click",function(){
+        
+        $('.mainexp--js').show();
+           
+            $('html, body').animate({
+                scrollTop: $(".mainexp--js").offset().top
+            },1000)
+            
+           
+        });
+        });
+    
+        $(document).ready(function(){
+            $('.contact--js').hide();
+            $('.navbar__a--js3').on("click",function(){
+            
+            $('.contact--js').show();
+               
+                $('html, body').animate({
+                    scrollTop: $(".contact--js").offset().top
+                },1000)
+                
+               
+            });
+            });
