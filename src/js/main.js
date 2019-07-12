60c6bd7b643e6c86c104b1589f44b41b050c495b
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-12 07:45:17 +0200

    feat: gulp

--- src/js/main.js
+++ src/js/main.js
@@ -2,92 +2,57 @@
 
 
 $(document).ready(function(){
-$('.navbar--js').hide();
-$('.start__button--js').on("click",function(){
-
-$('.navbar--js').show();
-   
+    
+    $('.navbar--js').hide();
+    $('.start__button--js').on("click",function(){
+    $('.navbar--js').show();
     $('html, body').animate({
         scrollTop: $('.navbar--js').offset().top
     },1000)
-    
-   
-});
-});
+    });
 
-$(document).on('scroll', function () {
     let scrollPos = $(document).scrollTop(); 
     let sectionOffset = $('.navbar--js').offset().top;
    
      if (scrollPos < sectionOffset) {
       $('.start--js').css('opacity', 1 - 1.9 * scrollPos / sectionOffset);
     }
-  });
 
-  $(document).ready(function(){
+
+
     $('.mainbio--js').hide();
     $('.navbar__a--js1').on("click",function(){
-    
-    $('.mainbio--js').show();
-       
+        $('.mainbio--js').show();
         $('html, body').animate({
             scrollTop: $(".mainbio--js").offset().top
         },1000)
-        
-       
-    });
     });
 
-    $(document).ready(function(){
-        $('.mainexp--js').hide();
-        $('.navbar__a--js2').on("click",function(){
-        
+
+
+    $('.mainexp--js').hide();
+    $('.navbar__a--js2').on("click",function(){
         $('.mainexp--js').show();
-           
-            $('html, body').animate({
-                scrollTop: $(".mainexp--js").offset().top
-            },1000)
-            
-           
-        });
-        });
+        $('html, body').animate({
+            scrollTop: $(".mainexp--js").offset().top
+        },1000)
+    });
+
     
-        $(document).ready(function(){
-            $('.contact--js').hide();
-            $('.navbar__a--js3').on("click",function(){
-            
-            $('.contact--js').show();
-               
-                $('html, body').animate({
-                    scrollTop: $(".contact--js").offset().top
-                },1000)
-                
-               
-            });
-            });
 
-
-
-
-
-
-
-
-
-        
-
-            $(document).ready(function(){
-                
-                $('.scroll').on("click",function(){
-                
-               
-                   
-                    $('html, body').animate({
-                        scrollTop: $(".navbar--js").offset().top
-                    },1000)
-                    
-                   
-                });
-                });
+    $('.contact--js').hide();
+    $('.navbar__a--js3').on("click",function(){
+        $('.contact--js').show();
+        $('html, body').animate({
+        scrollTop: $(".contact--js").offset().top
+        },1000)
+    });
+    
+    $('.scroll').on("click",function(){
+        $('html, body').animate({
+            scrollTop: $(".navbar--js").offset().top
+        },1000)
+    });
+});
             
             
