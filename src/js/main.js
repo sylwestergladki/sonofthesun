3fda37489af012d0b9751d9d6f83a8ee4507ef11
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-15 16:09:02 +0200

    feat: delete navigation

--- src/js/main.js
+++ src/js/main.js
@@ -3,56 +3,23 @@
 
 $(document).ready(function(){
     
-    $('.navbar--js').hide();
+    $('.main--js').hide();
+    
     $('.start__button--js').on("click",function(){
-    $('.navbar--js').show();
+    $('.main--js').show();
+ 
     $('html, body').animate({
-        scrollTop: $('.navbar--js').offset().top
+        scrollTop: $('.main--js').offset().top
     },1000)
     });
 
     let scrollPos = $(document).scrollTop(); 
-    let sectionOffset = $('.navbar--js').offset().top;
+    let sectionOffset = $('.main--js').offset().top;
    
      if (scrollPos < sectionOffset) {
       $('.start--js').css('opacity', 1 - 1.9 * scrollPos / sectionOffset);
     }
 
-
-
-    $('.mainbio--js').hide();
-    $('.navbar__a--js1').on("click",function(){
-        $('.mainbio--js').show();
-        $('html, body').animate({
-            scrollTop: $(".mainbio--js").offset().top
-        },1000)
-    });
-
-
-
-    $('.mainexp--js').hide();
-    $('.navbar__a--js2').on("click",function(){
-        $('.mainexp--js').show();
-        $('html, body').animate({
-            scrollTop: $(".mainexp--js").offset().top
-        },1000)
-    });
-
-    
-
-    $('.contact--js').hide();
-    $('.navbar__a--js3').on("click",function(){
-        $('.contact--js').show();
-        $('html, body').animate({
-        scrollTop: $(".contact--js").offset().top
-        },1000)
-    });
-    
-    $('.scroll').on("click",function(){
-        $('html, body').animate({
-            scrollTop: $(".navbar--js").offset().top
-        },1000)
-    });
 });
             
             
