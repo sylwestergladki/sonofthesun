fe10cc86589b4d09a81e4fd12184215560d8cf19
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-17 18:50:58 +0200

    feat: slide and opacity animation JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,9 @@
+const startbutton = document.querySelector('.start__button--js');
+const main = document.querySelector('.main--js');
+const startMain = document.querySelector('.start');
 
 
-
-$(document).ready(function(){
-    
-    $('.main--js').hide();
-    
-    $('.start__button--js').on("click",function(){
-    $('.main--js').show();
- 
-    $('html, body').animate({
-        scrollTop: $('.main--js').offset().top
-    },1000)
-    });
-
-    let scrollPos = $(document).scrollTop(); 
-    let sectionOffset = $('.main--js').offset().top;
-   
-     if (scrollPos < sectionOffset) {
-      $('.start--js').css('opacity', 1 - 1.9 * scrollPos / sectionOffset);
-    }
-
+startbutton.addEventListener('click', (e) =>{
+    main.classList.toggle('main--animation');
+    startMain.classList.toggle('start--animation');
 });
-            
-            
