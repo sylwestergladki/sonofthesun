4c7ec1a54c8e418a8da9b8d12273e36304ec65cf
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-18 17:34:28 +0200

    feat: JS, CSS ZOOMPHOTO

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { brotliDecompressSync } from "zlib";
+
 const startbutton = document.querySelector('.start__button--js');
 const main = document.querySelector('.main--js');
 const startMain = document.querySelector('.start');
@@ -6,4 +8,28 @@ const startMain = document.querySelector('.start');
 startbutton.addEventListener('click', (e) =>{
     main.classList.toggle('main--animation');
     startMain.classList.toggle('start--animation');
+});
+
+const photoFirst = document.querySelector('.main__image--js');
+const zoomPhotoFirst = document.querySelector('.photo-zoom');
+const bigPhotoFirst = document.querySelector('.photo-zoom__image--js')
+
+photoFirst.addEventListener('click', (e) =>{
+    zoomPhotoFirst.classList.add('photo-zoom--visible');
+});
+
+bigPhotoFirst.addEventListener('click', (e) =>{
+    zoomPhotoFirst.classList.remove('photo-zoom--visible');
+});
+
+const photoSecond = document.querySelector('.main-second__image--js');
+const zoomPhotoSecond = document.querySelector('.photo-zoom-second');
+const bigPhotoSecond = document.querySelector('.photo-zoom-second__image--js');
+
+photoSecond.addEventListener('click', (e) =>{
+    zoomPhotoSecond.classList.add('photo-zoom--visible');
+});
+
+bigPhotoSecond.addEventListener('click', (e) =>{
+    zoomPhotoSecond.classList.remove('photo-zoom--visible');
 });
